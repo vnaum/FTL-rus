b05b1c8cc293ff91259c4c8244b9b0a8e8e1296d
Vladislav Naumov <vnaum@vnaum.com>
2013-06-05 22:29:26 +0700

    no dups

--- dumpstrings.py
+++ dumpstrings.py
@@ -3,5 +3,10 @@
 from BeautifulSoup import BeautifulSoup
 soup = BeautifulSoup(open('events-raw.xml').read())
 
+printed = {}
 for t in soup.findAll('text'):
-    print t.text
+    tx = t.text
+    if len(tx)==0: continue
+    if printed.has_key(tx): continue
+    print tx.encode('utf-8')
+    printed[tx] = 1
