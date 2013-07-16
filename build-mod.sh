21970cbc2f19ad68cb330daadeea2e30ed0766f5
Vladislav Naumov <vnaum@vnaum.com>
2013-07-16 22:48:52 +0700

    mod builder

---
+++ build-mod.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+# build mod file
+rm -rf build-tmp
+mkdir build-tmp
+
+# copy static files:
+cp -r resources/* build-tmp
+
+mkdir -p build-tmp/data
+for f in orig-xml/*.xml
+do
+  BN=`basename $f`
+  POFILE="po/$BN.po"
+  echo "translating $f + $POFILE -> build-tmp/data/$BN"
+  ./apply-po $POFILE < $f > build-tmp/data/$BN
+done
+
+TESTGAMEDIR=$HOME/games/FTL_rus_test
+
+cd build-tmp
+zip $TESTGAMEDIR/data/mods/rus_test.ftl *
+cd $TESTGAMEDIR/data
+# ./modman
