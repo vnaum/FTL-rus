3fbe31ef9b0d584aa7aa294af2fd7b1897fac3d2
Vladislav Naumov <vnaum@vnaum.com>
2013-09-15 00:19:19 +0700

    decoder

---
+++ notab/decode-notabenoid.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -pn
+# vim: fileencoding=koi8-r
+s/%%/£/g;
+tr/\x01\x08\x07\x11\x02\x05\x03\x0b\x0c`\x24\x1c\x1e\x1d\x1b\x16\x7b\x7e\x5c\x18\x23\x1a\x1f\x19\x17\x40\x7d\x5e\x5c\x2a\x7c6/גנלשחיהזדÙÔÊÌËÉ×ØÑÛÄÎÚÍÖÇÞÀÝÜÃÜÂ/;
+tr/ypoanCceTBxOHMKPAEX/ÕÒÏÁÐףÓÅפקÈןמםכעבוט/;
+tr/\x09<&/ץ\-Æ/;
+s//ü/g;
+s//‏/g;
+s//ס/g;
+s/_/‗/g;
+s/>/"/g;
+s//"/g;
+s//צ/g;
+s//‏/g;
+s//û/g;
+s//‎/g;
+
+# fix this back:
+s/FפL/FTL/g;
